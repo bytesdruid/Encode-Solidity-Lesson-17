fb4d5b4b5b2c545f42d85f66d080195169e5b7f5
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-14 11:04:57 -0600

    Completed hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   paths: { tests: "tests" },
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        runs: 10000,
+        enabled: true
+      }
+    }
+  }
 };
 
 export default config;
